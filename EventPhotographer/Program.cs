320ed149683b2e9b7f59b861514ec620ec987e55
aivaras <kriksciunasaivaras@gmail.com>
2026-05-09 17:28:32 +0200

    remove hsts

--- EventPhotographer/Program.cs
+++ EventPhotographer/Program.cs
@@ -35,13 +35,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Production
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseExceptionHandler("/Error");
 
 app.UseRouting();
