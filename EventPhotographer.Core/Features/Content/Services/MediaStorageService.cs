f51e103bb568e7d6cd9cc0035c663a858cc13c98
aivaras <kriksciunasaivaras@gmail.com>
2026-06-14 20:37:27 +0200

    Remove hardcoded bucket name

--- EventPhotographer.Core/Features/Content/Services/MediaStorageService.cs
+++ EventPhotographer.Core/Features/Content/Services/MediaStorageService.cs
@@ -1,21 +1,26 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using EventPhotographer.Core.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace EventPhotographer.Core.Features.Content.Services;
 
 public class MediaStorageService(
-    IAmazonS3 s3Client)
+    IAmazonS3 s3Client, 
+    IOptions<ObjectStorageConfiguration> _options)
 {
-    private const string BucketName = "event-photographer";
+    private readonly ObjectStorageConfiguration options = _options.Value;
 
     public async Task<string> UploadFile(Stream fileStream, string contentType, string key)
     {
         await s3Client.PutObjectAsync(new PutObjectRequest
         {
-            BucketName = BucketName,
+            BucketName = options.BucketName,
             Key = key,
             InputStream = fileStream,
             ContentType = contentType,
+            DisablePayloadSigning = true,
+            DisableDefaultChecksumValidation = true,
         });
 
         return key;
@@ -25,7 +30,7 @@ public class MediaStorageService(
     {
         return await s3Client.GetObjectAsync(new GetObjectRequest
         {
-            BucketName = BucketName,
+            BucketName = options.BucketName,
             Key = key,
         });
     }
